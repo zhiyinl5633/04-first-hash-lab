8fd3bbd049f0a49119d6872d9cacfda806f82887
Kelly Lai <zhiyinl5633@hstat.org>
2017-11-03 15:37:57 +0000

    fix errors

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+  my_hash = {"p1" => "study hall", "p2" => "ap physics", "p3" => "english", "p4" => "qol", "p5" => "sep", "p6" => "ap stats", "p7" => "yoga", "p8" => "lunch"}
 end
 
 
@@ -7,20 +8,21 @@ def shipping_manifest
   #set a variable called `shipping_manifest`, equal to a hash
   #fill that hash with key/value pairs that describe the following information: 
   #We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-  
+  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 end
 
 def retrieval
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
   #your code here!
-  
+  return shipping_manifest["oil paintings"]
 end
 
 def adding
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
   #your code here
   #remember to return the shipping_manifest hash
-  
+  shipping_manifest["pearl necklace"] = 1
+  shipping_manifest
 end
 
 
